bfc85022935572483cce47b28113a42f226c3574
sawharsha <lakshmansaisriharshakurumella@gmail.com>
2025-10-17 17:00:32 +0530

    updated UI and modified src files

--- src/BirthdayWish.jsx
+++ src/BirthdayWish.jsx
@@ -72,7 +72,7 @@ const BirthdayCard = () => {
       <div className="confetti-container"></div>
       <div className="card">
         <h1 className="birthday-title">🎉 Happy Birthday!</h1>
-        <h2 className="subtitle">🎂✨❤ @BUJJI #Nanna #Bujjilu #Swathodu #Doctor_Amma #kittodu #Bangaram #MY LIFE #MY_EVERYTHING...🫴🫀👸🏼
+        <h2 className="subtitle">🎂✨  ABHIGNA_GARU
 </h2>
 
         {!opened ? (
@@ -82,44 +82,16 @@ const BirthdayCard = () => {
           </div>
         ) : (
           <div className="card-content">
-            <p>Just wanted to remind you—you’re my favorite person 💖. Naku nee B'day ante naku chela istam naku pedda festival..🎊🥳
-Nee next B'Day aena nitho ne pakka vundi celebrate chesukovali ani vundhi..🥹🤗 </p>
-            <p className="middle">Niko vishyam telusa potti nenu ndhuku lv chesanu telusaa ninnu nv namedha chupinchina premaki padipoyane niku..💘👸.<br />
-Parichayam Chalu Anukunna 🤝,<br/>
-Prapancham Aypoyavu 🌍❤.<br/>
-Kasepu Chusi Agipodam Anukunna 👀⏸,<br/>
-Prema Aypoyavu 💘.<br/>
-Koncham Samyam Ichhi Chudam Anukunna ⏳🤔,<br/>
-Sarvasvam Aypoyavu 💞🙏.<br/>
-Life long nitho brathakali Anukunnanu 💑🌟,<br/>
-Vadhilesi Vellipoyavu 💔🚶‍♀.<br/>
-<br/><br/>
-Ika medha na lyf veru kadha 🚫❤<br/>
-Andhulo nv vundav 😔💔<br/>
-Eppudu kavalante appudu ninnu chudalenu 👀❌<br/>
-Nee matalu vinnalenu 🎧🚫<br/>
-Niku nachela edhi cheyalenu 🤷‍♂💭<br/>
-Idhantha naaa valla avuthundhaaa 😢🤯
-</p>
+            <p>Happy Birthday_@ABHIGNA_GARU🎂 
+ </p>
+            <p className="middle">I hope all your dreams and goals come true soon ✨ May every wish you make today come true 🎂 You deserve the world 🌍 </p>
             {/* <p>May every wish you make today come true. You deserve the world, and I’ll always be here to remind you of that.</p> */}
             {/* <p className="closing">Let’s always stay like this… together, forever 🫶</p> */}
           </div>
         )}
         <div className="last"> 
-          <p>“Niku eppudaina life lo emaina problem vachina 💔 <br/>
-Ledha ontariga vunnanu anipinchina 😔<br/>
-Niku ee problem vachina nee mundhu addamga nilabadipothaanu 💪✨<br/>
-Ee world motham niku opposite gaa vunna nenu matram nethone vuntanu 🤗❤<br/>
-Just naku Hi ani msg cheyu chalu 📩🙂<br/>
-Nee msg kosam eppudu wait chesthune vuntanu ⏳💙<br/>
-</p>
-          <p className="closing">Dr. Amma_garu meremo jagrathaga vundali andi 😢🙏<br/>
-Baga chadhivesukovali 📚💡<br/>
-Tym ki ammu thinneyali 🍲⏰ and tym ki bajjovali ☕🥪<br/>
-Manushulatho jagrathaga vundali 🧍‍♀⚠ sare naa… 😔❤<br/>
-Miss u so much Bujji 😭💖<br/>
-Love u 💕🤗”
- 🫶</p>
+          <p>Always stay strong 💪, keep smiling 😊, and never stop lifting 🏋‍♀ </p>
+          <p className="closing">You truly deserve all the good things in life 😊</p>
         </div>
         <audio ref={audioRef} src={birthdaySong} />
       </div>
